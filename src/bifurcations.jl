795ab8e782c7a7caa14ee3de8dc64a51748a2947
Gaussia <torkel.loman@gmail.com>
2018-11-08 16:41:27 +0000

    Miscelenoues changes

--- src/bifurcations.jl
+++ src/bifurcations.jl
@@ -1,8 +1,34 @@
 import RecipesBase.plot, RecipesBase.plot!
 
+struct bifurcation_diagram
+    param::Symbol
+    param_values::Vector{Float64}
+    fixed_points::Array{Float64,2}
+    stabilities::Array{Tuple{Bool,Bool},2}
+    colors::Array{color,2}
+end
+
+struct bifurcation_diagram_2d
+    param1::Symbol
+    param2::Symbol
+    param_values::Matrix{Float64}
+    fixed_points::Matrix{Array{Float64,2}}
+    stabilities::Matrix{Array{Tuple{Bool,Bool},2}}
+    colors::Matrix{Array{color,2}}
+end
+
 function bifurcation_color(stability)
     stability[1]&&stability[2]&&(return :yellow)
     stability[1]&&(return :red)
     stability[2]&&(return :green)
     return :blue
+end
+
+function plot(bifurcation_diagram)
+end
+function plot!(bifurcation_diagram)
+end
+function plot(bifurcation_diagram_2d)
+end
+function plot!(bifurcation_diagram_2d)
 end
